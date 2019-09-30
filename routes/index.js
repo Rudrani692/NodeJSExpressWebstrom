d390ccb4d3e1639d527f7fb013ee7c1f89f316e4
vrudrani <rvannemreddy@horizon.csueastbay.edu>
2019-09-30 15:06:31 -0700

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 //**************************************************************************
 //***** mongodb get all of the Routes in Routes collection where frequence>=1
 //      and sort by the name of the route.  Render information in the views/pages/mongodb.ejs
-app.get('/mongodb', function (request, response) {
+router.get('/mongodb', function (request, response) {
 
   mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
     if(err) throw err;
